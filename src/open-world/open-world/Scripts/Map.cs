82218e0c0fc9233af5980831b4f1d3c08a6cc7a2
vladimir-siv <vladimir.sivcev@gmail.com>
2020-02-24 01:04:20 +0100

    Sync

--- src/open-world/open-world/Scripts/Map.cs
+++ src/open-world/open-world/Scripts/Map.cs
@@ -11,6 +11,61 @@ namespace open_world
 {
 	public sealed class Map : IDisposable
 	{
+		public static void Generate
+		(
+			string mapName,
+			vec3 pos_from, vec3 pos_to,
+			vec3 rot_from, vec3 rot_to,
+			vec3 scl_from, vec3 scl_to,
+			int object_count,
+			params string[] object_names
+		)
+		{
+			if (mapName.Contains("/") || mapName.Contains("\\")) throw new FormatException("File name cannot have subdirectories.");
+
+			var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			var path = Path.Combine(desktop, mapName + ".map");
+
+			var rnd = new Random();
+
+			float RndFloat(float from, float to) => from + (to - from) * (float)rnd.NextDouble();
+
+			using (var stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+			{
+				using (var writer = new StreamWriter(stream))
+				{
+					for (var i = 0; i < object_count; ++i)
+					{
+						var name = object_names[rnd.Next(object_names.Length)];
+
+						var yd = 0.0f;
+						if (name == "crate") yd += 0.5f;
+
+						var pos = new vec3
+						(
+							RndFloat(pos_from.x, pos_to.x),
+							RndFloat(pos_from.y + yd, pos_to.y + yd),
+							RndFloat(pos_from.z, pos_to.z)
+						);
+						var rot = new vec3
+						(
+							RndFloat(rot_from.x, rot_to.x),
+							RndFloat(rot_from.y, rot_to.y),
+							RndFloat(rot_from.z, rot_to.z)
+						);
+						var scl = new vec3
+						(
+							RndFloat(scl_from.x, scl_to.x),
+							RndFloat(scl_from.y, scl_to.y),
+							RndFloat(scl_from.z, scl_to.z)
+						);
+
+						writer.WriteLine($"{name}:{pos.x},{pos.y},{pos.z}:{rot.x},{rot.y},{rot.z}:{scl.x},{scl.y},{scl.z}");
+					}
+				}
+			}
+		}
+
 		private Stream CurrentMap = null;
 		private StreamReader Reader = null;
 
@@ -65,64 +120,6 @@ namespace open_world
 			Reader = null;
 			CurrentMap.Dispose();
 			CurrentMap = null;
-		}
-	}
-
-	public static class MapGenerator
-	{
-		public static void Generate
-		(
-			string mapName,
-			vec3 pos_from, vec3 pos_to,
-			vec3 rot_from, vec3 rot_to,
-			vec3 scl_from, vec3 scl_to,
-			int object_count,
-			params string[] object_names
-		)
-		{
-			if (mapName.Contains("/") || mapName.Contains("\\")) throw new FormatException("File name cannot have subdirectories.");
-
-			var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-			var path = Path.Combine(desktop, mapName + ".map");
-
-			var rnd = new Random();
-
-			float RndFloat(float from, float to) => from + (to - from) * (float)rnd.NextDouble();
-
-			using (var stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
-			{
-				using (var writer = new StreamWriter(stream))
-				{
-					for (var i = 0; i < object_count; ++i)
-					{
-						var name = object_names[rnd.Next(object_names.Length)];
-
-						var yd = 0.0f;
-						if (name == "crate") yd += 0.5f;
-
-						var pos = new vec3
-						(
-							RndFloat(pos_from.x, pos_to.x),
-							RndFloat(pos_from.y + yd, pos_to.y + yd),
-							RndFloat(pos_from.z, pos_to.z)
-						);
-						var rot = new vec3
-						(
-							RndFloat(rot_from.x, rot_to.x),
-							RndFloat(rot_from.y, rot_to.y),
-							RndFloat(rot_from.z, rot_to.z)
-						);
-						var scl = new vec3
-						(
-							RndFloat(scl_from.x, scl_to.x),
-							RndFloat(scl_from.y, scl_to.y),
-							RndFloat(scl_from.z, scl_to.z)
-						);
-
-						writer.WriteLine($"{name}:{pos.x},{pos.y},{pos.z}:{rot.x},{rot.y},{rot.z}:{scl.x},{scl.y},{scl.z}");
-					}
-				}
-			}
 		}
 	}
 
